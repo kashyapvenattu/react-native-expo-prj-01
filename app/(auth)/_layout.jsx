672a27f1a8983d8559a0f90b661451e099c64d93
Kashyap <kashyapvenattu@gmail.com>
2024-05-16 15:33:08 -0600

    update sign-in sign-up

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,7 +1,14 @@
 import { Redirect, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
+// import { Loader } from "../../components";
+// import { useGlobalContext } from "../../context/GlobalProvider";
+
 const AuthLayout = () => {
+  // const { loading, isLogged } = useGlobalContext();
+
+  // if (!loading && isLogged) return <Redirect href="/home" />;
+
   return (
     <>
       <Stack>
@@ -18,6 +25,8 @@ const AuthLayout = () => {
           }}
         />
       </Stack>
+
+      {/* <Loader isLoading={loading} /> */}
       <StatusBar backgroundColor="#161622" style="light" />
     </>
   );
